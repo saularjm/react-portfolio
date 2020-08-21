cd34e21f7c93f8c7c0cc0b5e687ad1d6cf13553b
Saular Moaddeli <jake.moaddeli@gmail.com>
2020-08-20 18:02:48 -0700

    added file/folder structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Wrapper from "./components/Wrapper";
+import About from "./pages/About";
+import Portfolio from "./pages/Portfolio";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Header />
+        <Wrapper>
+          <Route exact path="/" component={About} />
+          <Route exact path="/portfolio" component={Portfolio} />
+        </Wrapper>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Header/index.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Header() {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-info mb-3">
+            <span className="navbar-brand mb-1 h1">Saular Moaddeli</span>
+            <div className="ml-auto" id="navbarNavAltMarkup">
+            <div className="navbar-nav">
+            <a className="nav-item">
+            <Link
+              to="/"
+              className={
+                window.location.pathname === "/"
+                  ? "nav-link active"
+                  : "nav-link"
+              }
+            >
+              About
+            </Link>
+            </a>
+          <a className="nav-item">
+          <Link
+              to="/portfolio"
+              className={
+                window.location.pathname === "/portfolio"
+                  ? "nav-link active"
+                  : "nav-link"
+              }
+            >
+              Portfolio
+            </Link>
+              </a>
+        </div>
+      </div>
+  </nav>
+    );
+}
+
+export default Header;
